0c1848a242716ec4553d0e872302c95b7b887684
Tejal Daga <90956881+TejalRD@users.noreply.github.com>
2026-02-18 10:43:22 -0500

    Time: 0 ms (100.00%) | Memory: 44.6 MB (94.85%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,19 @@
+//dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+class Solution {
+    public int minCostClimbingStairs(int[] cost) 
+    {
+        int n = cost.length;
+        int prev2=cost[0];
+        int prev1=cost[1];
+
+        for(int i=2; i<n; i++)
+        {
+            int curr = cost[i] + Math.min(prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+
+        }
+        return Math.min(prev1,prev2);
+        
+    }
+}
